983b76f08627d5c0b4fd06f8ebf8978a055a4746
Rick Gallegos <duulketariakan@yahoo.com>
2018-03-04 19:44:24 -0800

    Adds family members cleanly

--- hhbuilder/index.js
+++ hhbuilder/index.js
@@ -9,10 +9,10 @@ const relationship = document.querySelector('select');
 const smoker = document.querySelector('input[name="smoker"]');
 
 const household = [];
+let index = 0;
 
 // CURRENT TODO:
-// - Change preview div member as they are being typed into the form
-// - Add button clears the preview div and adds it to the household ol
+
 // - Household li has span button to remove that member
 // - JSON.stringify() the household array and append to ".debug" on submit
 //   - check if household isn't empty first before sending
@@ -21,7 +21,7 @@ const household = [];
 addButton.type = 'button';
 
 // // Delete these
-// console.dir(form);
+console.dir(form);
 // console.dir(addButton);
 // console.dir(house);
 
@@ -59,20 +59,62 @@ function checkSmoke() {
   }
 }
 
+function clearForm() {
+  console.log('Reset form works: ', form.reset);
+  document.querySelector('ul.preview li.age').textContent = 'Age: ';
+  document.querySelector('ul.preview li.relationship').textContent = 'Relationship: ';
+  document.querySelector('ul.preview li.smoker').textContent = 'Habit: Non-smoker';
+  form.reset();
+}
+
 function handleAdd(e) {
   const member = {};
   console.log('add button clicked');
 
   if (checkAge() && checkRelationship()) {
-    member.age = age.value;
-    member.relationship = relationship.value;
-    member.smoker = checkSmoke();
-    console.log('Currrent member: ', member);
+    const addAge = age.value;
+    const addRelationship = relationship.value;
+    const addSmoker = checkSmoke();
+    // const preview = document.querySelector('ul.preview').cloneNode(true);
+    const html = `
+      <li data-index=${index}>
+        <strong>Family Member</strong>
+        <ul class="family" data-index="${index}">
+            <li class="age">Age: ${age.value}</li>
+            <li class="relationship">Relationship: ${relationship.value}</li>
+            <li class="smoker">Habit: ${checkSmoke()}</li>
+        </ul>
+      </li>
+    `
+
+    member.age = addAge;
+    member.relationship = addRelationship;
+    member.smoker = addSmoker;
+    member.index = index;
+    household.push(member);
+
+    house.insertAdjacentHTML('beforeend', html);
+
+    index++;
+    clearForm();
   }
 }
 
 function handlePreviewChange(e) {
-  console.log('What changed: ', e);
+  const type = e.target.type;
+
+  if (type === 'text') {
+    const pAge = document.querySelector('ul.preview li.age');
+    pAge.textContent = 'Age: ' + e.target.value;
+  } else if (type === 'select-one') {
+    const pRel = document.querySelector('ul.preview li.relationship');
+    pRel.textContent = 'Relationship: ' + e.target.value;
+  } else if (type === 'checkbox') {
+    const pSmoke = document.querySelector('ul.preview li.smoker');
+    pSmoke.textContent = 'Habit: ' + checkSmoke();
+  } else {
+    console.error('Invalid event type: ', e.target);
+  }
 }
 
 function handleSubmit(e) {
@@ -83,11 +125,12 @@ function handleSubmit(e) {
 
 function initialize() {
   const html = `
-    <ol class="preview">
-      <li class="age">${age.value}</li>
-      <li class="relationship">${relationship.value}</li>
-      <li class="smoker">${checkSmoke()}</li>
-    </ol>
+    <h3>Family Member Preview</h3>
+    <ul class="preview" data-index="${index}">
+        <li class="age">Age: ${age.value}</li>
+        <li class="relationship">Relationship: ${relationship.value}</li>
+        <li class="smoker">Habit: ${checkSmoke()}</li>
+    </ul>
   `
   builder.insertAdjacentHTML('beforeend', html);
 }
